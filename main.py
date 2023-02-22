d796ca9359e29f0643c60cdc2cc45c9ec9235ad9
OpenY0urEyes <110251749+OpenY0urEyes@users.noreply.github.com>
2023-02-22 11:03:54 +0300

    2

---
+++ main.py
@@ -0,0 +1,49 @@
+# a = 4
+# b = 6
+#
+# one = (a - b) * (a + b)
+# print(one)
+# one = a**2 - 2*a*b +b**2
+# print(one)
+# one = a**2+2*a*b+b**2
+# print(one)
+# one = a**3-3*a**2*b+3*a*b**2-b**3
+# print(one)
+# one = a**3 + 3 * a**2 * b + 3 * a * b**2 - b**3
+# print(one)
+# one = (a + b)*(a**2 - a*b + b**2)
+# print(one)
+# one = (a - b)*(a**2 - a*b + b**2)
+# print(one)
+import math
+from functools import reduce
+
+#---------------
+
+
+#
+# a = 3
+# b = 4
+# c = 5
+#
+# f1 = a*a
+# f2 = a+c
+# f3 = f2 ** 2
+# g1 = f1 + f3
+# print("g1: ", g1)
+# h1 = 10*b
+# h2 = g1 / h1
+#
+# j1 = c ^ 2
+# j2 = a*j1
+# j3 = 4 * j2
+#
+# k1 = h2 - j3
+# print( k1)
+#
+# er = -1/2
+# finish = k1**er
+# print(finish)
+
+
+#--------------
