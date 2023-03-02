4e761355d61611c9315133aea04d206036d7f5f2
OpenY0urEyes <110251749+OpenY0urEyes@users.noreply.github.com>
2023-03-02 14:46:39 +0300

    4

--- main.py
+++ main.py
@@ -1,50 +1,32 @@
-import math
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import numpy as np
-#
-a = int(input("a = "))
-b = int(input("b = "))
-c = int(input("c = "))
+ArifmOrGeom = int(input("0 - арифметиеская прогрессия / 1 - геометрическая"))
+FormulaOrCicle = int(input("0 - формула / 1 - цикл"))
 
-#
-# d = b**2 - 4*a*c
-# print(math.sqrt(d))
-#
-# if d < 0:
-#     print("Нет корней")
-# elif d == 0:
-#     x = -(b/2*a)
-#     print("x = ", x)
-# else:
-#     x_1 = (-b + math.sqrt(d))/(2*a)
-#     x_2 = (-b - math.sqrt(d))/(2*a)
-#     print("Первый X = ",x_1)
-#     print("Второй X = ",x_2)
-"""
-x = []
-y = []
-count = int(input("введите количество x"))
-for i in range(0, count):
-    x.append(float(input()))
+firstNumber = int(input("Первое число"))
+raznoct = int(input("Разность"))
+lastNumber = int(input("Последнее число"))
+num = firstNumber
 
-print(x)
-for i in range(len(x)):
-    y.append(((a*x[i])**2) + (b*x[i]) + c)
+#------Arifm------
+if ArifmOrGeom == 0:
 
-print("y = ", y)
-
-fig, ax = plt.subplots()  # Create a figure containing a single axes.
-ax.plot(x, [1, 4, 2, 3])  # Plot some data on the axes.
-
-plt.show()"""
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.linspace(-10, 10, 1000)
-y = x**2 + 2*x + 2
-
-fig, ax = plt.subplots()
-ax.plot(x, y)
-plt.show()
+    if FormulaOrCicle == 0:
+            #---Formula---
+        while firstNumber < lastNumber:
+            firstNumber += raznoct
+            print(firstNumber)
+    elif FormulaOrCicle == 1:
+    #---Cicle---
+        for i in range(firstNumber, lastNumber, raznoct):
+            print(i)
+elif ArifmOrGeom == 1:
+    if FormulaOrCicle == 0:
+            #---Formula---
+        while firstNumber < lastNumber:
+            firstNumber *= raznoct
+            print(firstNumber)
+    elif FormulaOrCicle == 1:
+            #---Cicle---
+        for i in range(firstNumber, lastNumber):
+            if num < i:
+                num *= raznoct
+                print(num)
